7e29e4818fcb9e8473c1215a0392e7e84bc331d3
anderrf <anderson_rocha_farias@outlook.com>
2024-09-23 00:27:01 -0300

    Creation of api call

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { Transactions } from "./pages/Transactions";
 import { GlobalStyle } from "./styles/global.ts"
+import { TransactionsProvider } from "./contexts/TransactionsContext.tsx";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Transactions/>
+      <TransactionsProvider>
+        <Transactions/>
+      </TransactionsProvider>
       <GlobalStyle/>
     </ThemeProvider>
   )
